dd7b520cf43d8810117ac007e63db58281b8d99f
tuchongyang <779311998@qq.com>
2021-04-26 18:30:24 +0800

    update

---
+++ src/views/cart/components/CartTab/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="cube-tab-bar border-bottom-1px">
+        <div class="cube-tab cube-tab_active">
+            <i class=""></i><div>商品</div>
+        </div>
+        <div class="cube-tab"><i class=""></i><div>评论</div></div><div class="cube-tab"><i class=""></i><div>商家</div></div><div class="cube-tab-bar-slider" style="width: 138px; transform: translateX(0px) translateZ(0px);"></div>
+        </div>
+</template>
