ec541580fc133733f541134af4c48448cdfa9631
tuchongyang <779311998@qq.com>
2021-05-08 18:19:37 +0800

    优化tab切换

---
+++ src/views/cart/components/CartContainer/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="cart-container">
+        <div class="container shop">
+            
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.cart-container{
+    position: fixed;
+    top: 180px;
+    bottom: 0;
+    .container{
+        position: relative;
+        height: 100%;
+    }
+}
+</style>
--- src/views/cart/components/CartTab/index.vue
+++ src/views/cart/components/CartTab/index.vue
@@ -1,8 +1,41 @@
 <template>
-    <div class="cube-tab-bar border-bottom-1px">
-        <div class="cube-tab cube-tab_active">
-            <i class=""></i><div>商品</div>
+    <div class="tab-bar border-bottom-1px">
+        <div class="tab" :class="{tab_active: index==tab}" v-for="(item,index) in tabs" :key="index" @click="select(index)">
+            <div>{{item}}</div>
         </div>
-        <div class="cube-tab"><i class=""></i><div>评论</div></div><div class="cube-tab"><i class=""></i><div>商家</div></div><div class="cube-tab-bar-slider" style="width: 138px; transform: translateX(0px) translateZ(0px);"></div>
-        </div>
+        <div class="tab-bar-slider" :style="{width: '33.333333%', left: tab*33.333333+'%'}"></div>
+    </div>
 </template>
+<script>
+export default {
+    data(){
+        return {
+            tab: 0,
+            tabs:['商品','评论','商家']
+        }
+    },
+    methods:{
+        select(i){
+            this.tab = i
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.tab-bar{
+    display: flex;
+    position: relative;
+    .tab{
+        flex: 1;
+        text-align: center;
+        line-height: 40px;
+    }
+    .tab-bar-slider{
+        position: absolute;
+        height: 2px;
+        background:red;
+        bottom: 0;
+        transition: left .3s;
+    }
+}
+</style>
